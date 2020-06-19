95a4b7443323e8ad77356dcc2d6a277f9559f2f4
admayra <67140076+admayra@users.noreply.github.com>
2020-06-19 12:21:01 +0530

    Add app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+// buttons
+const prevButton = document.querySelector('#prevButton');
+const nextButton = document.querySelector('#nextButton');
+
+// counter
+let counter = 1;
+let size = carouselImages[0].clientWidth;
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+// Button Listeners
+nextButton.addEventListener('click', () => {
+  if(counter >= carouselImages.length - 1) return;
+  carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+  counter++;
+  size = carouselImages[counter --].clientWidth;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevButton.addEventListener('click', () => {
+  if(counter <= 0) return;
+  carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+  counter--;
+  size = carouselImages[counter --].clientWidth;
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+// Slide Effect
+carouselSlide.addEventListener('transitionend', () => {
+  if (carouselImages[counter].id === 'lastClone') {
+    carouselSlide.style.transition = 'none';
+    counter = carouselImages.length - 2;
+    size = carouselImages[counter --].clientWidth;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+  if (carouselImages[counter].id === 'firstClone') {
+    carouselSlide.style.transition = 'none';
+    counter = carouselImages.length - counter;
+    size = carouselImages[counter --].clientWidth;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+});
